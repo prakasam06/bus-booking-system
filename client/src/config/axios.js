4d99c9b552df5193a8def4f7c0ce4d00d7a3c456
prakasam b <prakasamb2021@gmail.com>
2024-04-21 22:22:56 +0530

    bug fix

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
 
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://bus-booking-system.vercel.app/api/v1/",
   withCredentials: true,
   responseType: "json",
 });
