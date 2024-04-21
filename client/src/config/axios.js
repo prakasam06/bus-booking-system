8fc70adf656301f85b02da1fa08afbce508288bc
prakasam b <prakasamb2021@gmail.com>
2024-04-21 21:54:49 +0530

    changed axios config

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -3,10 +3,9 @@ import axios from "axios";
 
 const instance = axios.create({
 
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://bus-booking-system.onrender.com/api/v1/",
   withCredentials: true,
   responseType: "json",
-  
 });
 
 export default instance;
