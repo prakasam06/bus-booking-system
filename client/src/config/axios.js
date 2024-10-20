e933ff426a9d2e7778bc744a1c75f694ed5caa70
Prakasam B <prakasamb2021@gmail.com>
2024-10-20 23:15:31 +0530

    changed FE base url for deployment - EC2

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://bus-booking-system.onrender.com/api/v1/",
+  baseURL: "http://35.170.11.221:8000/api/v1/",
   withCredentials: true,
   responseType: "json",
 });
